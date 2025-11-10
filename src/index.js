521b7b8de382d3c071c3fa3b98054f59ba5837fc
TakenoShoya <22ts072@m.nanzan-u.ac.jp>
2025-11-10 12:19:15 +0900

    rebuild event list

--- src/index.js
+++ src/index.js
@@ -783,19 +783,21 @@ client.on(Events.InteractionCreate, async (interaction) => {
         events[interaction.guildId].push(ev);
         saveEvents(events);
 
+        // 🔔 ログチャンネルへ作成通知
+                await postToLogChannel(interaction.client, interaction.guildId, [
+                  '🗓️ **予定追加**',
+                  `【日付】${isoUTC ? DateTime.fromISO(isoUTC).setZone(ZONE).toFormat('yyyy-LL-dd HH:mm') + ' (JST)' : '未設定'}`,
+                  `【シナリオ名】${scenario}`,
+                  `【システム名】${system || '未設定'}`,
+                  `【GM名】<@${interaction.user.id}>`,
+                  `【部屋】<#${privateChannelId}>`,
+                  `ID:\`${ev.id}\``
+                ].join('\n'));
+
         // 掲示板を更新（最新版1件維持）
         await updateEventBoardMessage(interaction.client, interaction.guildId);
 
-        // 🔔 ログチャンネルへ作成通知
-        await postToLogChannel(interaction.client, interaction.guildId, [
-          '🗓️ **予定追加**',
-          `【日付】${isoUTC ? DateTime.fromISO(isoUTC).setZone(ZONE).toFormat('yyyy-LL-dd HH:mm') + ' (JST)' : '未設定'}`,
-          `【シナリオ名】${scenario}`,
-          `【システム名】${system || '未設定'}`,
-          `【GM名】<@${interaction.user.id}>`,
-          `【部屋】<#${privateChannelId}>`,
-          `ID:\`${ev.id}\``
-        ].join('\n'));
+
 
         // 作成者へエフェメラル返信
         await interaction.reply({
